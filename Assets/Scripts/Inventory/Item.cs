584d93d3810cffdb05b90df51ee6c020c34eb46a
Mathis Normand <2060485@collegemv.qc.ca>
2024-09-26 11:47:36 -0400

    Interaction objet

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -6,12 +6,27 @@ public class Item : MonoBehaviour
 {
     [SerializeField] 
     private Inventory inventory;
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private float activationDistance = 1.5f;
+    [SerializeField]
+    private Canvas canvas;
 
-    private void OnCollisionEnter(Collision other) {
-        if (other.collider.CompareTag("Player")) {
-            inventory.AddItem(gameObject.name);
-            Debug.Log("Item picked up");
-            Destroy(gameObject);
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance <= activationDistance){
+            canvas.enabled = true;
+            if (Input.GetKeyDown("e")){
+                inventory.AddItem(gameObject.name);
+                Debug.Log("Item picked up");
+                Destroy(gameObject);
+            }
+        }
+        else{
+            canvas.enabled = false;
         }
     }
 
