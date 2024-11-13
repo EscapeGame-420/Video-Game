f7a03ac84712d3a0c77452f1d3a164c006eeb73f
Mathis Normand <2060485@collegemv.qc.ca>
2024-11-13 01:13:43 -0500

    enigme 1

---
+++ Assets/Scripts/Objects/MathisEnigme.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathisEnigme : MonoBehaviour
+{
+    private string prefabPath = "UI/Canvas";
+    private int enigmeCounter = 0;
+    private Canvas canvas;
+
+    [SerializeField]
+    private SpriteRenderer bigFlame;
+
+    [SerializeField]
+    private GameObject fire;
+
+    [SerializeField]
+    private Animator candleAnimator;
+
+    [SerializeField]
+    private float activationDistance = 1.5f;
+
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private Vector3 canvasOffset = new Vector3(0, 1, 0);
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject canvaToAdd = Resources.Load<GameObject>(prefabPath);
+        GameObject newObject = Instantiate(canvaToAdd, transform.position, transform.rotation);
+        newObject.transform.SetParent(transform);
+        canvas = GetComponentInChildren<Canvas>();
+        canvas.gameObject.AddComponent<LookAtCam>();
+        canvas.transform.position = transform.position + canvasOffset;
+        bigFlame.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (candleAnimator.GetInteger("candleCount") == 2)
+        {
+            bigFlame.enabled = true;
+        }
+        else
+        {
+            if (distance <= activationDistance)
+            {
+                canvas.enabled = true;
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (gameObject.CompareTag("Good") && fire.activeSelf)
+                    {
+                        int enigmeCounter = candleAnimator.GetInteger("candleCount");
+                        fire.SetActive(false);
+                        enigmeCounter--;
+                        candleAnimator.SetInteger("candleCount", enigmeCounter);
+                        Debug.Log(enigmeCounter);
+                    }
+                    else if (gameObject.CompareTag("Good"))
+                    {
+                        int enigmeCounter = candleAnimator.GetInteger("candleCount");
+                        fire.SetActive(true);
+                        enigmeCounter++;
+                        candleAnimator.SetInteger("candleCount", enigmeCounter);
+                        Debug.Log(enigmeCounter);
+                    }
+                    else if (gameObject.CompareTag("Bad") && fire.activeSelf)
+                    {
+                        int enigmeCounter = candleAnimator.GetInteger("candleCount");
+                        fire.SetActive(false);
+                        enigmeCounter++;
+                        candleAnimator.SetInteger("candleCount", enigmeCounter);
+                        Debug.Log(enigmeCounter);
+                    }
+                    else if (gameObject.CompareTag("Bad"))
+                    {
+                        int enigmeCounter = candleAnimator.GetInteger("candleCount");
+                        fire.SetActive(true);
+                        enigmeCounter--;
+                        candleAnimator.SetInteger("candleCount", enigmeCounter);
+                        Debug.Log(enigmeCounter);
+                    }
+                }
+            }
+            else
+            {
+                canvas.enabled = false;
+            }
+        }
+
+
+    }
+}
