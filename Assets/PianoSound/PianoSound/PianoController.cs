66df49f1326c07901bd92eea5ac8a1b372969439
AyaIssa1 <2139923@collegemv.qc.ca>
2024-12-02 16:39:42 -0500

    ajout piano

---
+++ Assets/PianoSound/PianoSound/PianoController.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PianoController : MonoBehaviour
+{
+    public GameObject canvasToOpen;  
+    public GameObject promptCanvas;  
+    public KeyCode keyToPress = KeyCode.E;  
+    public MonoBehaviour cameraController; 
+    public Transform player; 
+    public Transform target;  
+    public float activationDistance = 2f;
+
+    public GameObject pianoKey;  
+
+    
+    public Button[] pianoButtons;  
+    private int currentButtonIndex = 0;  
+
+    private bool isCanvasActive = false;  
+
+    void Start()
+    {
+      
+        pianoKey.SetActive(false);
+
+        for (int i = 0; i < pianoButtons.Length; i++)
+        {
+            int buttonIndex = i;  
+            pianoButtons[i].onClick.AddListener(() => OnPianoButtonClick(buttonIndex));
+        }
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, target.position);
+
+        
+        if (distance <= activationDistance)
+        {
+            promptCanvas.SetActive(true);  
+
+           
+            if (Input.GetKeyDown(keyToPress))
+            {
+                ToggleCanvas(); 
+            }
+        }
+        else
+        {
+            promptCanvas.SetActive(false);  
+        }
+    }
+
+    void ToggleCanvas()
+    {
+        if (!isCanvasActive)
+        {
+            canvasToOpen.SetActive(true);  
+            pianoKey.SetActive(false);  
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            cameraController.enabled = false; 
+        }
+        else
+        {
+            canvasToOpen.SetActive(false); 
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            cameraController.enabled = true; 
+        }
+
+      
+        isCanvasActive = !isCanvasActive;
+    }
+
+  
+    void OnPianoButtonClick(int buttonIndex)
+    {
+      
+        if (buttonIndex == currentButtonIndex)
+        {
+            currentButtonIndex++;  
+
+            if (currentButtonIndex == pianoButtons.Length)
+            {
+               
+                pianoKey.SetActive(true);
+                CloseCanvas();  
+            }
+        }
+        else
+        {
+          
+            currentButtonIndex = 0; 
+            pianoKey.SetActive(false);  
+            CloseCanvas();  
+        }
+    }
+
+    void CloseCanvas()
+    {
+        canvasToOpen.SetActive(false);  
+        isCanvasActive = false; 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraController.enabled = true;  
+    }
+}
