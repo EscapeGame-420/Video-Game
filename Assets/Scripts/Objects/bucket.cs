29edcc75c7c3c4aef18b1992b2d4500d65cb7fae
Tri tran dang minh <111996897+Fingernamedkid@users.noreply.github.com>
2024-12-03 10:52:39 -0500

    fixing bugs

--- Assets/Scripts/Objects/bucket.cs
+++ Assets/Scripts/Objects/bucket.cs
@@ -8,7 +8,7 @@ public class Bucket : MonoBehaviour
     public Transform player;
     public float activationDistance = 7.0f;
     public bool isCandleNear = false;
-    private bool canvasCreated = false; // Flag to track if the canvas has been created
+    public bool canvasCreated = false; // Flag to track if the canvas has been created
     public float x =0;
     public float y =0;
     public float z =0;
@@ -16,7 +16,7 @@ public class Bucket : MonoBehaviour
     public Sprite sprite;
     public string itemName1;
     public Sprite sprite1;
-    private Transform canvasTransform; 
+    public Transform canvasTransform; 
     public GameObject goodeffect;
     public GameObject badeffect;
 
@@ -43,20 +43,8 @@ public class Bucket : MonoBehaviour
         float distance = Vector3.Distance(transform.position, player.position);
         Inventory inventory = FindFirstObjectByType<Inventory>();
         // Check if the canvas has already been created
-        if (!canvasCreated)
-        {
-            Item.CreateCanvas(this.gameObject);
-            canvasCreated = true; // Set the flag to true after creating the canvas
-
-            // Find the created canvas and set its position
-            canvasTransform = transform.Find(this.gameObject.name + "Canvas");
-            if (canvasTransform != null)
-            {
-                canvasTransform.localPosition = new Vector3(x, y, z); // Set the desired position
-            }
-        }
-
-        if (!(distance <= activationDistance && (inventory.IncludeItemName("bones")|| inventory.IncludeItemName("eyeball")|| mixItems.Count == 3)))
+        HandleCanvasCreation();
+        if (CanNotActivateCanvas(distance, inventory))
         {
             canvasTransform.gameObject.SetActive(false);
             return;
@@ -65,8 +53,17 @@ public class Bucket : MonoBehaviour
         {
             canvasTransform.gameObject.SetActive(true);
         }
-
+        CheckStatus();
         Debug.Log("Le joueur s'approche");
+        
+        if (Input.GetKeyDown("e") &&  !CanNotActivateCanvas(distance, inventory))
+        {
+            HandleInteraction(inventory);
+        }
+        
+    }
+    public void CheckStatus()
+    {
         if (mixItems.Count == 3)
         {
             mixItems.Sort();
@@ -84,46 +81,64 @@ public class Bucket : MonoBehaviour
             goodeffect.SetActive(false);
             badeffect.SetActive(false);
         }
-        if (Input.GetKeyDown("e"))
+    }
+    public void HandleCanvasCreation()
+    {
+        if (!canvasCreated)
         {
-            if (mixItems.Count == 3)
+            Item.CreateCanvas(this.gameObject);
+            canvasCreated = true;
+
+            canvasTransform = transform.Find(this.gameObject.name + "Canvas");
+            if (canvasTransform != null)
             {
-                mixItems.Sort();
-                switch (string.Join(",", mixItems))
-                {
-                    case "bones,eyeball,eyeball":
-                        Item item = new Item
-                        {
-                            itemName = itemName,
-                            sprite = sprite
-                        };
-                        inventory.AddItem(item);
-                        bucketObject.SetActive(false);
-                        break;
-                    default:
-                        Item defaultItem = new Item
-                        {
-                            itemName = itemName1,
-                            sprite = sprite1
-                        };
-                        inventory.AddItem(defaultItem);
-                        bucketObject.SetActive(false);
-                        break;
-                }
+                canvasTransform.localPosition = new Vector3(x, y, z); // Set the desired position
             }
-            else
+        }
+    }
+     public bool CanNotActivateCanvas(float distance, Inventory inventory)
+    {
+    return !(distance <= activationDistance && (inventory.IncludeItemName("bones")|| inventory.IncludeItemName("eyeball")|| mixItems.Count == 3));
+    }
+    public void HandleInteraction(Inventory inventory)
+    {
+        if (mixItems.Count == 3)
+        {
+            mixItems.Sort();
+            switch (string.Join(",", mixItems))
             {
-                if (inventory.IncludeItemName("bones") && inventory.IsSelectingItem("bones"))
-                {
-                    inventory.UseItem("bones");
-                    mixItems.Add("bones");
-                }
-                else if (inventory.IncludeItemName("eyeball") && inventory.IsSelectingItem("eyeball"))
-                {
-                    inventory.UseItem("eyeball");
-                    mixItems.Add("eyeball");
-                    
-                }
+                case "bones,eyeball,eyeball":
+                    Item item = new Item
+                    {
+                        itemName = itemName,
+                        sprite = sprite
+                    };
+                    inventory.AddItem(item);
+                    bucketObject.SetActive(false);
+                    break;
+                default:
+                    Item defaultItem = new Item
+                    {
+                        itemName = itemName1,
+                        sprite = sprite1
+                    };
+                    inventory.AddItem(defaultItem);
+                    bucketObject.SetActive(false);
+                    break;
+            }
+        }
+        else
+        {
+            if (inventory.IncludeItemName("bones") && inventory.IsSelectingItem("bones"))
+            {
+                inventory.UseItem("bones");
+                mixItems.Add("bones");
+            }
+            else if (inventory.IncludeItemName("eyeball") && inventory.IsSelectingItem("eyeball"))
+            {
+                inventory.UseItem("eyeball");
+                mixItems.Add("eyeball");
+                
             }
         }
     }
--- CodeCoverage/Report/Summary.md
+++ CodeCoverage/Report/Summary.md
@@ -1,32 +1,32 @@
 ﻿# Summary
 |||
 |:---|:---|
-| Generated on: | 2024-12-03 - 12:29:53 AM |
+| Generated on: | 2024-12-03 - 1:13:31 AM |
 | Parser: | MultiReportParser (4x OpenCoverParser) |
 | Assemblies: | 2 |
 | Classes: | 17 |
 | Files: | 17 |
-| Covered lines: | 342 |
-| Uncovered lines: | 643 |
-| Coverable lines: | 985 |
-| Total lines: | 1937 |
-| Line coverage: | 34.7% (342 of 985) |
+| Covered lines: | 411 |
+| Uncovered lines: | 586 |
+| Coverable lines: | 997 |
+| Total lines: | 1952 |
+| Line coverage: | 41.2% (411 of 997) |
 | Covered branches: | 0 |
 | Total branches: | 0 |
-| Covered methods: | 39 |
-| Total methods: | 81 |
-| Method coverage: | 48.1% (39 of 81) |
+| Covered methods: | 46 |
+| Total methods: | 85 |
+| Method coverage: | 54.1% (46 of 85) |
 
 |**Name**|**Covered**|**Uncovered**|**Coverable**|**Total**|**Line coverage**|**Covered**|**Total**|**Branch coverage**|**Covered**|**Total**|**Method coverage**|
 |:---|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|
-|**InvenReff**|**255**|**112**|**367**|**613**|**69.4%**|**0**|**0**|****|**25**|**32**|**78.1%**|
-|Inventory|78|45|123|182|63.4%|0|0||8|11|72.7%|
+|**InvenReff**|**256**|**111**|**367**|**613**|**69.7%**|**0**|**0**|****|**25**|**32**|**78.1%**|
+|Inventory|79|44|123|182|64.2%|0|0||8|11|72.7%|
 |InventoryManager|102|29|131|221|77.8%|0|0||10|10|100%|
 |Item|54|20|74|126|72.9%|0|0||4|5|80%|
 |ItemSlot|10|18|28|58|35.7%|0|0||1|4|25%|
 |LookAtCam|11|0|11|26|100%|0|0||2|2|100%|
-|**ObjectRef**|**87**|**531**|**618**|**1324**|**14%**|**0**|**0**|****|**14**|**49**|**28.5%**|
-|Bucket|0|80|80|131|0%|0|0||0|3|0%|
+|**ObjectRef**|**155**|**475**|**630**|**1339**|**24.6%**|**0**|**0**|****|**21**|**53**|**39.6%**|
+|Bucket|68|24|92|146|73.9%|0|0||7|7|100%|
 |Clock|0|41|41|65|0%|0|0||0|3|0%|
 |Door|0|37|37|66|0%|0|0||0|4|0%|
 |Frame|0|49|49|70|0%|0|0||0|3|0%|
